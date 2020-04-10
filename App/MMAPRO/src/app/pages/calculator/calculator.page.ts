c2da365ba3ab535d6460d6db0bec60876c351c37
Matt <mrmace@umail.iu.edu>
2020-04-09 20:53:09 -0400

    Added Bmi calculator/ updated styling

--- App/MMAPRO/src/app/pages/calculator/calculator.page.ts
+++ App/MMAPRO/src/app/pages/calculator/calculator.page.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorPage implements OnInit {
 
+  
+  height: number;
+  weight: number;
+  resultNumber = '00.0';
+  resultText = 'unknown';
+  icon = './assets/images/normal.png';
+  color = 'var(--ion-color-primary)';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  bmiCalculate() {
+
+    const height = this.height;
+    const weight = this.weight;
+
+    if (Number(height) && Number(weight)) {
+
+      const imc = ((weight / height / height) * 703);
+
+      this.resultNumber = imc.toFixed(2);
+
+      if (imc < 18.5) {
+        this.resultText = 'underweight';
+        this.color = '#22a6b3';
+        this.icon = './assets/images/underweight.png';
+      } else if (imc > 18.5 && imc < 25) {
+        this.resultText = 'normal weight';
+        this.color = '#6ab04c';
+        this.icon = './assets/images/normal.png';
+      } else if (imc >= 25 && imc < 30) {
+        this.resultText = 'overweight';
+        this.color = '#f0932b';
+        this.icon = './assets/images/overweight.png';
+      } else {
+        this.resultText = 'obese';
+        this.color = '#eb4d4b';
+        this.icon = './assets/images/obesity.png';
+      }
+
+    }
+
   }
 
 }
