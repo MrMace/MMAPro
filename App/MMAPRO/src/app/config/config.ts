a00f165a3930acfcc956f1fc76d0beb794f5e2e1
Matt <mrmace@umail.iu.edu>
2020-04-25 18:38:21 -0400

    config and strings pages

---
+++ App/MMAPRO/src/app/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    Url: 'https://mixedmartialartspro.com/backend',
+    Facebook: 'https://facebook.com',
+    Youtube: 'https://youtube.com',
+    Twitter: 'https://twitter.com',
+    Instagram: 'https://instagram.com',
+
+};
